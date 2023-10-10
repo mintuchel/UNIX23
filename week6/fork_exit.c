62f8b0df8a8bcf09fbcb685eb119d2285245993a
mintuchel <max3627@naver.com>
2023-10-10 19:31:34 +0900

    added comments

--- week6/fork_exit.c
+++ week6/fork_exit.c
@@ -3,6 +3,12 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+// it is safe to separate parent function and child function
+
+// there are 2 things to check when using fork()
+// 1. child must be separated to function and have exit(0);
+// 2. parent must have wait(0);
+
 void do_child(){
     printf("pid=%ld, gid=%ld, sid=%ld\n", getpid(), getpgid(0), getsid(getpid()));
     exit(0);
