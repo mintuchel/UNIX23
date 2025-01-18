bf1e4e998c502afb49923dd3365a818a056ee082
mintuchel <max3627@naver.com>
2025-01-18 22:09:29 +0900

    설계5 동기화 문제 수정

--- 설계과제/5.c
+++ 설계과제/5.c
@@ -18,162 +18,178 @@
 #include <sys/errno.h>
 
 #define BUFSIZE 512
-#define N 5
+#define N 20
 
-struct manage_buffer
+struct manager
 {
-    int front;
-    int back;
-
+    int frontIdx;
+    int rearIdx;
+    int nextId;
     int total_user;
-    int id_buffer[4];
 };
 
-struct message_buffer
+struct message
 {
     int read_counter;
-    int message_counter;
-
+    int message_id;
     int sender_id;
     char mtext[BUFSIZE];
 };
 
-void receiver(int id, int semid, int manage_shmid, int message_shmid)
+void receiver(int id, int message_id, int semid, int manager_shmid, int message_arr_shmid)
 {
     struct sembuf wait[] = {{1, -1, 0}, {2, -1, 0}};
-    struct sembuf signal[] = {{0, 1, 0}, {2, 1, 0}};
-    struct sembuf p_buf[] = {{1, 1, 0}, {2, 1, 0}};
+    struct sembuf signal1[] = {{0, 1, 0}, {2, 1, 0}};
+    struct sembuf signal2[] = {{1, 1, 0}, {2, 1, 0}};
+    struct manager *manager_shm;
+    struct message *message_arr_shm;
 
-    struct manage_buffer *manage;
-    struct message_buffer *message;
-    manage = (struct manage_buffer *)shmat(manage_shmid, 0, 0);
-    message = (struct message_buffer *)shmat(message_shmid, 0, 0);
-
-    int message_counter = 1;
+    manager_shm = (struct manager *)shmat(manager_shmid, 0, 0);
+    message_arr_shm = (struct message *)shmat(message_arr_shmid, 0, 0);
 
     while (1)
     {
+        /** critical section **/
         semop(semid, wait, 2);
 
-        if (message_counter == message[manage->back].message_counter)
+        if (message_id == message_arr_shm[manager_shm->frontIdx].message_id)
         {
-            printf("[receiver] %d : %s\n", message[manage->back].sender_id, message[manage->back].mtext);
-            message[manage->back].read_counter--;
-            message_counter++;
+            if (message_arr_shm[manager_shm->frontIdx].sender_id != id)
+            {
+                printf("[receiver] %d : %s\n", message_arr_shm[manager_shm->frontIdx].sender_id, message_arr_shm[manager_shm->frontIdx].mtext);
+            }
 
-            if (message[manage->back].read_counter == 0)
-                manage->back = (manage->back + 1) % N;
+            message_arr_shm[manager_shm->frontIdx].read_counter--;
+            message_id++;
+            
+            if (message_arr_shm[manager_shm->frontIdx].read_counter == 0)
+            {
+                manager_shm->frontIdx = (manager_shm->frontIdx + 1) % N;
+                semop(semid, signal1, 2);
+                continue;
+            }
+        }
 
-            semop(semid, signal, 2);
-        }
-        else
-        {
-            semop(semid, p_buf, 2);
-        }
+        semop(semid, signal2, 2);
+        /** critical section **/
     }
     return;
 }
 
-void sender(int id, int semid, int manage_shmid, int message_shmid)
+void sender(int id, int semid, int manager_shmid, int message_arr_shmid)
 {
     struct sembuf wait[] = {{0, -1, 0}, {2, -1, 0}};
     struct sembuf signal[] = {{1, 1, 0}, {2, 1, 0}};
+    struct manager *manager_shm;
+    struct message *message_arr_shm;
 
-    struct manage_buffer *manage;
-    struct message_buffer *message;
-    manage = (struct manage_buffer *)shmat(manage_shmid, 0, 0);
-    message = (struct message_buffer *)shmat(message_shmid, 0, 0);
+    manager_shm = (struct manager *)shmat(manager_shmid, 0, 0);
+    message_arr_shm = (struct message *)shmat(message_arr_shmid, 0, 0);
 
-    int message_counter = 0;
-    char buffer[BUFSIZE];
+    char input[BUFSIZE];
 
-    while (gets(buffer))
+    while (gets(input))
     {
+        /** critical section **/
         semop(semid, wait, 2);
 
-        message[manage->front].sender_id = id;
-        strcpy(message[manage->front].mtext, buffer);
-        message_counter += 1;
-        message[manage->front].message_counter = message_counter;
-        message[manage->front].read_counter = manage->total_user;
-        manage->front = (manage->front + 1) % N;
+        message_arr_shm[manager_shm->rearIdx].sender_id = id;
+        strcpy(message_arr_shm[manager_shm->rearIdx].mtext, input);
+        message_arr_shm[manager_shm->rearIdx].message_id = manager_shm->nextId;
+        message_arr_shm[manager_shm->rearIdx].read_counter = manager_shm->total_user;
+        manager_shm->nextId++;
+        manager_shm->rearIdx = (manager_shm->rearIdx + 1) % N;
 
         semop(semid, signal, 2);
+        /** critical section **/
     }
-
     return;
 }
 
 int main(int argc, char **argv)
 {
-    int i, id, semid, manage_shmid, message_shmid;
-    key_t semkey, message_shmkey, manage_shmkey;
+    int i, id;
+    int semid, manager_shmid, message_arr_shmid;
+    key_t semkey, manager_shmkey, message_arr_shmkey;
     pid_t pid[2];
 
     union semun arg;
-
-    // 1 = prevents overflow
-    // 2 = prevents underflow (number of items in the buffer that are available for consumption)
-    // 3 = control buffer access (mutual exclusion) amount of free space left in
     ushort buf[3] = {N, 0, 1};
-
-    struct manage_buffer *manage;
-    struct message_buffer *message;
+    struct manager *manage;
+    struct message *message;
 
     semkey = ftok("semkey", 3);
-    manage_shmkey = ftok("manage_shmkey", 3);
-    message_shmkey = ftok("message_shmkey", 3);
+    manager_shmkey = ftok("manage_shmkey", 3);
+    message_arr_shmkey = ftok("message_shmkey", 3);
 
     semid = semget(semkey, 3, 0600 | IPC_CREAT | IPC_EXCL);
-
     if (semid == -1)
     {
         semid = semget(semkey, 3, 0600);
-    }else{
+    }
+    else
+    {
         arg.array = buf;
         semctl(semid, 0, SETALL, arg);
     }
 
-    message_shmid = shmget(message_shmkey, N * sizeof(struct message_buffer), 0600 | IPC_CREAT);
-    message = (struct message_buffer *)shmat(message_shmid, 0, 0);
+    manager_shmid = shmget(manager_shmkey, sizeof(struct manager), 0600 | IPC_CREAT);
+    manage = (struct manager *)shmat(manager_shmid, 0, 0);
 
-    manage_shmid = shmget(manage_shmkey, sizeof(struct manage_buffer), 0600 | IPC_CREAT);
-    manage = (struct manage_buffer *)shmat(manage_shmid, 0, 0);
+    if (manage->total_user == 0)
+    {
+        manage->frontIdx = 0;
+        manage->rearIdx = 0;
+        manage->nextId = 1;
+        manage->total_user = 0;
+    }
+
+    message_arr_shmid = shmget(message_arr_shmkey, N * sizeof(struct message), 0600 | IPC_CREAT);
+    message = (struct message *)shmat(message_arr_shmid, 0, 0);
 
     id = atoi(argv[1]);
 
-    manage->total_user += 1;
+    struct sembuf w_buf = {2, -1, 0};
+    struct sembuf s_buf = {2, 1, 0};
+    int message_id;
 
-    debug(manage, message);
+    /** critical section **/
+    semop(semid, &w_buf, 1);
+
+    manage->total_user += 1;
+    message_id = manage->nextId;
+    semop(semid, &s_buf, 1);
+    
+    /** critical section **/
 
     printf("id : %d\n", id);
 
-    // receiver
     pid[0] = fork();
-    if (pid[0] == 0) {
-        receiver(id, semid, manage_shmid, message_shmid);
+    if (pid[0] == 0)
+    {
+        receiver(id, message_id, semid, manager_shmid, message_arr_shmid);
         exit(0);
     }
 
-    // sender
     pid[1] = fork();
-    if(pid[1] == 0){
-        sender(id, semid, manage_shmid, message_shmid);
+    if (pid[1] == 0)
+    {
+        sender(id, semid, manager_shmid, message_arr_shmid);
         exit(0);
     }
 
-    for (int i = 0; i < 2;i++){
+    for (int i = 0; i < 2; i++)
+    {
         wait(0);
     }
-    
-    manage->id_buffer[id - 1] = 0;
+
     manage->total_user -= 1;
 
     if (manage->total_user == 0)
     {
         semctl(semid, IPC_RMID, 0);
-        shmctl(manage_shmid, IPC_RMID, 0);
-        shmctl(message_shmid, IPC_RMID, 0);
+        shmctl(manager_shmid, IPC_RMID, 0);
+        shmctl(message_arr_shmid, IPC_RMID, 0);
     }
 
     exit(0);
